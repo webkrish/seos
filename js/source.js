b91b4e69bbb6c518bf009a147c7be0f2e059c305
Krishnendu Tripathy <krishnendutripathy337@gmail.com>
2023-06-20 10:45:15 +0530

    v1.1

--- js/source.js
+++ js/source.js
@@ -1,16 +1,16 @@
 
 
 
-$("button").click(function () {
-  $(".fa-times").toggle()
-  $(".fa-bars").toggle()
-  $("ul").toggle("slow")
-});
-
-// $("button").click(function(){
-//   $(".fa-times").toggle();
-//   $(".fa-bars").toggle();
-//   $("ul").animate({
-//       height: "toggle"
-//     });
+// $("button").click(function () {
+//   $(".fa-times").toggle()
+//   $(".fa-bars").toggle()
+//   $("ul").toggle("slow")
 // });
+
+$("button").click(function(){
+  $(".fa-times").toggle();
+  $(".fa-bars").toggle();
+  $("ul").animate({
+      height: "toggle"
+    });
+});
